8ac08558a2cf46f92533043e9f2f2c4cbe64379d
Spencer Creer <spencercreer@gmail.com>
2024-10-01 13:13:48 -0700

    FEAT: Client init

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-500">StrongMind Pizzeria</h1>
+      <a
+        href="https://github.com/spencercreer/StrongMind"
+        target="_blank"
+        className="w-12"
+      >
+        <img
+          src="https://www.google.com/url?sa=i&url=http%3A%2F%2Ft1.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcSVhJ46pOBVylg5_ZnYilSr14xSgJwSZ386f8C6hRKrA0MRiCpn2ozG-Bfcxa3bSdJ-&psig=AOvVaw0QC39jlPrge7Ldhu9F9fJj&ust=1727898967994000&source=images&cd=vfe&opi=89978449&ved=0CAwQjRxqFwoTCNCAsrP77YgDFQAAAAAdAAAAABAE"
+          alt="StrongMind Pizzeria logo"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default App;
