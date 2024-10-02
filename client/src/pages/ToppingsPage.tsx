810bd5e5d1fc2a56d4a38ec80091c5d909789519
Spencer Creer <spencercreer@gmail.com>
2024-10-02 16:44:59 -0700

    FEAT: Pages and routing

---
+++ client/src/pages/ToppingsPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ToppingsPage() {
+  return <div className="h-screen bg-tan">ToppingsPage</div>;
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#191B1F",
+        lightblue: "#3DBCD0",
+        red: "#FF2732",
+        yellow: "#FDBB12",
+        green: "#62C46F",
+        tan: "#D7D7C5",
+      },
+    },
   },
   plugins: [],
 };
